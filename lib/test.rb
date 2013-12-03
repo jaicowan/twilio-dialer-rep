8c4ee81603e5b62584108f99de5f1337e8fcbb28
jaicowan <jaicowan@yahoo.com.au>
2013-12-03 17:07:53 +0800

    Send SMS and listen

--- lib/test.rb
+++ lib/test.rb
@@ -2,6 +2,35 @@ require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
 
+# put your own credentials here - from twilio.com/user/account
+account_sid = 'AC49852d6f8f92a73cc6d9192ad5cd29e6'
+auth_token = '9a64bc17c6565c421cb0893b0ec99342'
+ 
+# set up a client to talk to the Twilio REST API
+@client = Twilio::REST::Client.new account_sid, auth_token
+ 
+#@call = @client.account.calls.create(
+#  :from => '+16466795828',   # From your Twilio number
+#  :to => '+61299598017',     # To any number
+#  # Fetch instructions from this URL when the call connects
+#  :url => 'http://twimlets.com/forward?PhoneNumber=+61299598017&FailUrl=http://myapp.com/please-try-later.mp3'
+#)
+ 
+from = "+16466795828" # Your Twilio number
+ 
+friends = {
+"+6584286950" => "SG Mobile",
+#"+14155557775" => "Boots",
+#"+14155551234" => "Virgil"
+}
+friends.each do |key, value|
+  client.account.messages.create(
+    :from => from,
+    :to => key,
+    :body => "Hey #{value}, Monkey party at 6PM. Bring Bananas!"
+  ) 
+  puts "Sent message to #{value}"
+end
  
 get '/sms-quickstart' do
   twiml = Twilio::TwiML::Response.new do |r|
