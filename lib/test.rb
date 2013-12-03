0e66282eccc8d2e3c42508655377d5241b193312
jaicowan <jaicowan@yahoo.com.au>
2013-12-03 17:02:16 +0800

    change test rb to listen

--- lib/test.rb
+++ lib/test.rb
@@ -1,9 +1,11 @@
 require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
- 
-get '/' do
-    'Hello World! Currently running version ' + Twilio::VERSION + \
-        ' of the twilio-ruby library. Sweet Hey?'
-end
 
+ 
+get '/sms-quickstart' do
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Message "Hey Monkey. Thanks for the message!"
+  end
+  twiml.text
+end
