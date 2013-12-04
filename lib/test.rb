6374caaa34f27ec5c53491537792df22f2b1c876
jaicowan <jaicowan@yahoo.com.au>
2013-12-04 12:57:51 +0800

    fix indents

--- lib/test.rb
+++ lib/test.rb
@@ -13,22 +13,22 @@ get '/send_to' do
 	num = "+" + params[:num]
 	name = params[:name]
 	client.account.messages.create(
-	:from => from, 
-	:to => num,
-	:body => "Hi #{name}, we would like to tell you about our promotional offer on widgets. Please reply 'accept' to have our consultant call you."
+		:from => from, 
+		:to => num,
+		:body => "Hi #{name}, we would like to tell you about our promotional offer on widgets. Please reply 'accept' to have our consultant call you."
 	) 
 	puts "Sent message to #{name}"
 end
 
 get '/sms-response' do
-    twiml = Twilio::TwiML::Response.new do |r|
+	twiml = Twilio::TwiML::Response.new do |r|
 		if  params[:Body] == "Accept"
 			(
 			r.Message "Thanks for your response. Our representative will be calling you shortly."
 			call = client.account.calls.create(
-			:from => from,   
-			:to => "+61299598017", 
-			:url => 'http://twimlets.com/forward?PhoneNumber=+14083313300&FailUrl=http://myapp.com/please-try-later.mp3'
+				:from => from,   
+				:to => "+61299598017", 
+				:url => 'http://twimlets.com/forward?PhoneNumber=+14083313300&FailUrl=http://myapp.com/please-try-later.mp3'
 			)
 			)
 		else
