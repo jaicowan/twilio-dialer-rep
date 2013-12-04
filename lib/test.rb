50ac215721c788ddbfc627bdf8a8fd1e950581d5
jaicowan <jaicowan@yahoo.com.au>
2013-12-04 13:08:12 +0800

    remove brackets

--- lib/test.rb
+++ lib/test.rb
@@ -15,7 +15,7 @@ get '/send_to' do
 	client.account.messages.create(
 		:from => from, 
 		:to => num,
-		:body => "Hi #{name}, we would like to tell you about our promotional offer on widgets. Please reply 'accept' to have our consultant call you."
+		:body => "Hi #{name}, we would like to tell you about our promotional offer on widgets. Please reply 'Accept' to have our consultant call you."
 	) 
 	puts "Sent message to #{name}"
 end
@@ -24,13 +24,11 @@ get '/sms-response' do
 	twiml = Twilio::TwiML::Response.new do |r|
 		num = params[:From]
 		if  params[:Body] == "Accept"
-			(
 			r.Message "Thanks for your response. Our representative will be calling you shortly."
 			call = client.account.calls.create(
 				:from => from,   
 				:to => num, 
 				:url => 'http://twimlets.com/forward?PhoneNumber=+14083313300&FailUrl=http://myapp.com/please-try-later.mp3'
-			)
 			)
 		else
 		# Do not call customer, send message body and subscriber CLID to invalid response bucket
